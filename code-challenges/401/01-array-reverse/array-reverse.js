81153eb42b6fd06e32b37a5f436ba0724d633b99
James <Jayms.Portune@gmail.com>
2020-01-25 07:04:01 -0800

    CC1 Completed

--- code-challenges/401/01-array-reverse/array-reverse.js
+++ code-challenges/401/01-array-reverse/array-reverse.js
@@ -1,3 +1,11 @@
 'use strict'
 
-module.exports = {}
+const reverseArr = (arr) => {
+    let reverse = [];
+    for(let i = arr.length - 1; i >= 0; i--){
+        reverse.push(arr[i]);
+    }
+    return reverse;
+}
+
+module.exports = {reverseArr};
--- code-challenges/401/01-array-reverse/array-reverse.test.js
+++ code-challenges/401/01-array-reverse/array-reverse.test.js
@@ -1,12 +1,13 @@
+let arrayReverse = require('./array-reverse.js');
+
 // -------------------------------------------------------------------
 //
 // Tests to confirm that the array recieved is in reverse order from 
 // recieved
 //
 // -------------------------------------------------------------------
-
 describe('Reverse Array', () => {
     it('Array that was recieved it in reverse order', () => {
-        expect(reverseArr).toStrictEqual(answer);
+        expect(arrayReverse.reverseArr([1,2,3])).toStrictEqual([3,2,1]);
     });
 });
